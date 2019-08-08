166d3728727919338f01b9090201c94fc1ac4b81
Caitlin Haaf <caitlinhaafillustration@gmail.com>
2019-08-08 11:31:27 -0400

    - pullReqEvents and forkEvents are now being filtered before storing in state - Search form and search results list now display separately based on state

--- haaf-personalproject/src/App.js
+++ haaf-personalproject/src/App.js
@@ -5,27 +5,25 @@ import './App.css'
 import Button from './components/button.js'
 import EventList from './components/eventList'
 
-// USING TEST API DATA
-// import {events} from './testresults-events'
-
 class App extends React.Component {
 
   state = {
     searchVisible: true,
-    events: [],
+    pullReqEvents: [],
+    forkEvents: [],
     searchID: "", 
-    btnDisable: true,
+    submitDisable: true,
     isLoading: false,
     hasError: false,
     errorMsg: ""
   };
 
+  // Helper Functions
   searchSubmit = () => {
     fetch(`https://api.github.com/users/${this.state.searchID}/events`)
       // check status of request before rendering
       .then(res => {
         const status = res.status;
-        // console.log("Server Status", status)
         if (status === 200) return res.json();
         else if(status === 404) throw new Error("Invalid user ID - try another name.");
         else throw new Error("Server not found - please try again later.");
@@ -34,95 +32,105 @@ class App extends React.Component {
         result => {
           this.setState({
             events: result,
+            pullReqEvents: result.filter( event => (
+              event.type === "PullRequestEvent"
+            )).reduce( (accumulator, event) => {
+              accumulator=[...accumulator, {
+                id: event.id,
+                repoName: event.payload.pull_request.title,
+                repoUrl: event.payload.pull_request.html_url,
+                pullReqStatus: event.payload.pull_request.state
+              }]
+              return accumulator
+            }, []),
+            forkEvents: result.filter( event => (
+              event.type === "ForkEvent"
+             )).reduce( (accumulator, event) => {      
+              accumulator=[...accumulator, {
+                id: event.id,
+                repoName: event.payload.forkee.full_name,
+                repoUrl: event.payload.forkee.html_url,
+                forkedFrom: event.repo.name
+              }]
+              return accumulator  
+            }, []),
             hasError: false,
-            errorMsg: ""
+            errorMsg: "",
+            searchVisible: false
           });
-          console.log("API SUCCESS", this.state)
         },
         error => {
           this.setState({
             hasError: true,
             errorMsg: error.message
           });
-          console.log("API ERROR", this.state)
         }
       );
   }
 
   searchUpdate = (evt) => {
     const searchTxt = evt.target.value;
-    const btnDisable = (searchTxt.length > 0) ? false : true;
+    const submitDisable = (searchTxt.length > 0) ? false : true;
     this.setState({
       searchID: searchTxt,
-      btnDisable: btnDisable
+      submitDisable: submitDisable
     })
   }
 
+  backToSearch = () => {
+    this.setState({
+      searchVisible : true, 
+      pullReqEvents: [],
+      forkEvents: [],
+      searchID: "",
+      submitDisable: true
+    })
+  }
+
+  // Render
   render(){
-    // filter pull request events
-    const pullRequestEvents = this.state.events.filter( event => (
-      event.type === "PullRequestEvent"
-    )).reduce( (accumulator, event) => {
-      accumulator=[...accumulator, {
-        id: event.id,
-        repoName: event.payload.pull_request.title,
-        repoUrl: event.payload.pull_request.html_url,
-        pullReqStatus: event.payload.pull_request.state
-      }]
-      return accumulator
-    }, []);
-
-    // filter fork events
-    const forkEvents = this.state.events.filter( event => (
-      event.type === "ForkEvent"
-     )).reduce( (accumulator, event) => {      
-      accumulator=[...accumulator, {
-        id: event.id,
-        repoName: event.payload.forkee.full_name,
-        repoUrl: event.payload.forkee.html_url,
-        forkedFrom: event.repo.name
-      }]
-      return accumulator
-    }, []);
-
     return (
-      <div>
-        {/* if state search visible, display search form... */}
-        {/* <SearchForm /> */}
-        <section className="searchForm">
-          <h3>Github User:</h3>
-          <input 
-            onChange={this.searchUpdate}
-            className={(!this.state.hasError) ? "searchInput" : "searchInput error"}
-            type="text" 
-            name="userName"/>
-          
-          { this.state.hasError &&
-              <p className="errorMsg">{this.state.errorMsg}</p>
-          }
+      <section>
+        {
+          this.state.searchVisible ? (
+            <section className="searchForm">
+              <h3>Github User:</h3>
+              <input 
+                onChange={this.searchUpdate}
+                className={(!this.state.hasError) ? "searchInput" : "searchInput error"}
+                type="text" 
+                name="userName"/>
+              
+              { this.state.hasError &&
+                  <p className="errorMsg">{this.state.errorMsg}</p>
+              }
 
-          <Button 
-            buttonTxt="GET USER" 
-            clickEvt={this.searchSubmit}
-            disabled={this.state.btnDisable}
-            />
-        </section>
-  
-        {/* else if state search visible is false, display result data... */}
-        <section className="searchResults">
-          <h1 className="searchUser">{this.state.searchID}</h1>
-  
-          <EventList 
-            header="Recent Forks"
-            events={forkEvents}/>
-  
-          <EventList 
-            header="Recent Pull Requests"
-            events={pullRequestEvents}/>
+              <Button 
+                buttonTxt="GET USER" 
+                clickEvt={this.searchSubmit}
+                disabled={this.state.submitDisable}
+                />
+            </section>
+          ) : (
+            <section className="searchResults">
+              <Button 
+                  buttonTxt="&larr; Back to Search"
+                  clickEvt={this.backToSearch}/>
 
-        </section>
+                <h1 className="searchUser">{this.state.searchID}</h1>
+        
+                <EventList 
+                  header="Recent Forks"
+                  events={this.state.forkEvents}/>
+        
+                <EventList 
+                  header="Recent Pull Requests"
+                  events={this.state.pullReqEvents}/>
 
-      </div>  
+              </section>
+          )
+        }
+      </section>  
     );
   }
   
